23b975c6dde76b02516ddf0ecd5eaf7ccd0aed8c
MichelyFelix <michelyvitoriauzl@gmail.com>
2023-07-08 18:45:31 -0300

    questao_3

---
+++ ponteiros/questao_3.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main(void) {
+ int a, b, c, d;
+ int *p1;
+ int *p2 = &a;//'p2' é inicializado e recebe o endereço da variável 'a'
+ int *p3 = &c;//'p3' é inicializado e recebe o e endereço da variável 'c'
+ p1 = p2;//'p1' recebe o valor de 'p3'
+ *p2 = 10;//o valor que 'p2' aponta é alterado para '10'
+ b = 20;
+ int **pp;//declaração de ponteiro para ponteiro
+ pp = &p1;//o ponteiro rece o endereço de 'p1'
+ *p3 = **pp;//a endereço que 'p3' aponta é atribuido com valor que 'pp' aponta
+ int *p4 = &d;//é declarado um ponteiro de inteiro que é inicializado com endereço de 'd'
+ *p4 = b + (*p1)++;//p4 recebe o valor de 'b'(20) e soma com o valor de 'p1'(10) e depois 'p1' é incrementado
+ printf("%d\t%d\t%d\t%d\n", a, b, c, d);
+ return 0;
+}
