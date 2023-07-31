50ceb2afe15abe80b591430d44384931b2b7d91f
MichelyFélix <129334724+MichelyFelix@users.noreply.github.com>
2023-07-31 08:50:56 -0300

    Update questao_3.c

--- ponteiros/questao_3.c
+++ ponteiros/questao_3.c
@@ -12,6 +12,6 @@ int main(void) {
  *p3 = **pp;//a endereço que 'p3' aponta é atribuido com valor que 'pp' aponta, ou seja, p1 (a)
  int *p4 = &d;//é declarado um ponteiro de inteiro que é inicializado com endereço de 'd'
  *p4 = b + (*p1)++;//p4 recebe o valor de 'b'(20) e soma com o valor de 'p1'(10) e depois 'p1' é incrementado
- printf("%d\t%d\t%d\t%d\n", a, b, c, d);
+ printf("%d\t%d\t%d\t%d\n", a, b, c, d);//11 20 10 30
  return 0;
 }
