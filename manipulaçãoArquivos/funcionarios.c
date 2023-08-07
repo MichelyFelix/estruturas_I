579aa21a4e22f4a5286769d070c4d03833105160
MichelyFelix <michelyvitoriauzl@gmail.com>
2023-08-07 11:32:02 -0300

    funcionarios

---
+++ manipulaçãoArquivos/funcionarios.c
@@ -0,0 +1,54 @@
+#include<stdio.h>
+#include<string.h>
+#include<stdlib.h>
+
+typedef struct funcionario{
+    char nome[100];
+    char funcional[10];
+    float salario;
+    char departamento;
+}Funcionario;
+
+void copia_dados(FILE* fl, int n, Funcionario** pessoal){
+*pessoal = (Funcionario*)malloc(n*sizeof(Funcionario));
+
+int contador;
+for(contador = 0; contador < n; contador++){
+fscanf(fl, "%s\t%[^\t]\t%c\t%f", (*pessoal)[contador].funcional, (*pessoal)[contador].nome, &(*pessoal)[contador].departamento, &(*pessoal)[contador].salario);
+}
+}
+
+void imprime_folha_pagamento(int n, Funcionario** pessoal, char depto){
+    float total = 0;
+    for(int contador = 0; contador < n; contador++){
+        if(pessoal[contador]->departamento == depto){
+           total = total + (*pessoal)[contador].salario;
+        }
+    }
+    printf("DEPARTAMENTO:%c\nVALOR TOTAL:R$ %.2f",depto, total);
+}
+
+int main(void){
+FILE* arquivo;
+int num_funcionarios;
+Funcionario*funcionarios;
+
+arquivo = fopen("funcionarios.txt","r");
+if(arquivo==NULL){
+    printf("Erro ao abrir arquivo!");
+    exit(1);
+}
+
+fscanf(arquivo, "%d", &num_funcionarios);//ve a quantidade de funcionários que tem no arquivo
+funcionarios = (Funcionario*)malloc(num_funcionarios*sizeof(Funcionario));
+
+copia_dados(arquivo,num_funcionarios,&funcionarios);
+fclose(arquivo);
+
+    imprime_folha_pagamento(num_funcionarios, &funcionarios, 'A');
+    imprime_folha_pagamento(num_funcionarios, &funcionarios, 'B');
+    imprime_folha_pagamento(num_funcionarios, &funcionarios, 'C');
+
+    free(funcionarios);
+    return 0;
+}
