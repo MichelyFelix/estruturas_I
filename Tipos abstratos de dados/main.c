18133cc9f3a4b3cdd77047b219579af238cacfca
MichelyFelix <michelyvitoriauzl@gmail.com>
2023-08-14 15:34:47 -0300

    tad

---
+++ Tipos abstratos de dados/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include "ponto.h"
+
+int main(void){
+    Ponto* p = pto_cria(2.0,1.0);
+    Ponto* q = pto_cria(3.4,2.1);
+    float d = pto_distancia(p,q);
+    printf("distancia entre pontos: %f\n", d);
+    pto_libera(q);
+    pto_libera(p);
+    return 0;
+}
